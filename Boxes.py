f4ddd2db56fec30985a6d9cd85553a3e4b121c3a
frangmat <frangmat@gmail.com>
2018-02-23 11:04:58 -0600

    Lazy ass mother fucker

--- Boxes.py
+++ Boxes.py
@@ -17,3 +17,46 @@
 #  Date Created: 02/23/18
 
 #  Date Last Modified: 02/23/18
+
+def does_fit (box1, box2):
+  return (box1[0] < box2[0]) and (box1[1] < box2[1]) and (box1[2] < box2[2])
+
+def main():
+  # open file for reading
+  in_file = open ('boxes.txt', 'r')
+
+  # read the number of boxes
+  line = in_file.readline()
+  line = line.strip()
+  num_boxes = int(line)
+
+  # create empty list of boxes
+  box_list = []
+
+  # read the list of boxes from file
+  for i in range (num_boxes):
+    line = in_file.readline()
+    line = line.strip()
+    box = line.split()
+    for i in range (len(box)):
+      box[i] = int (box[i])
+    box.sort()
+    box_list.append (box)
+
+  # close the file
+  in_file.close()
+
+  # sort the box list
+  box_list.sort()
+  print (box_list)
+
+  # create a list that will hold the nested boxes
+
+  # create a variable for the size of the nested boxes
+
+  # get all subsets of boxes
+
+  # for each subset check if they all fit
+
+  # add to list
+main()
