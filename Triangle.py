a7196d6f88d84f9719ae038e162c99f18849f9bb
frangmat <frangmat@gmail.com>
2018-03-07 10:45:34 -0600

    Created exhaustive search (i think)

--- Triangle.py
+++ Triangle.py
@@ -11,7 +11,21 @@
 import time
 # returns the greatest path sum using exhaustive search
 def exhaustive_search (grid):
-  return
+  sum_list=[]
+  sum_list=sum_exhaustive(grid, grid[0][0], 0, 0, sum_list)
+  return max(sum_list)
+
+def sum_exhaustive(grid, partial, row, position, sum_list):
+  hi = len(grid)
+  if (row+1 == hi):
+    sum_list.append(partial)
+  else:
+    c = partial
+    partial+=grid[row+1][position]
+    c+=grid[row+1][position+1]
+    sum_exhaustive (grid, partial, row+1, position, sum_list)
+    sum_exhaustive (grid, c, row+1, position+1, sum_list)
+  return sum_list
 
 # returns the greatest path sum using greedy approach
 def greedy (grid):
@@ -28,33 +42,63 @@ def dynamic_prog (grid):
 # reads the file and returns a 2-D list that represents the triangle
 def read_file ():
   return 
-
+  
 def main ():
   # read triangular grid from file
+  in_file= open('triangle.txt', 'r')
+   # read the number of rows
+  line = in_file.readline()
+  line = line.strip()
+  num_rows = int(line)
+
+  # create empty list of rows
+  row_list = []
+
+  # read the list of rows from file
+  for i in range (num_rows):
+    line = in_file.readline()
+    line = line.strip()
+    row = line.split()
+    for i in range (len(row)):
+      row[i] = int (row[i])
+    row_list.append (row)
+  print(row_list)
+  print()
 
   ti = time.time()
-  # output greates path from exhaustive search
+  # output greatest path from exhaustive search
+  great_sum=exhaustive_search(row_list)
+  print("The greatest path sum through exhaustive search is "+ str(great_sum) + '.')
   tf = time.time()
   del_t = tf - ti
   # print time taken using exhaustive search
-
+  print('The time taken for exhaustive search is '+str(del_t)+'seconds.')
+  print()
   ti = time.time()
-  # output greates path from greedy approach
+  # output greatest path from greedy approach
+  great_sum=greedy(row_list)
+  print("The greatest path sum through greedy search is "+ str(great_sum) + '.')
   tf = time.time()
   del_t = tf - ti
   # print time taken using greedy approach
-
+  print('The time taken for greedy search is '+str(del_t)+'seconds.')
+  print()
   ti = time.time()
   # output greates path from divide-and-conquer approach
+  great_sum=rec_search(row_list)
+  print("The greatest path sum through recursive search is "+ str(great_sum) + '.')
   tf = time.time()
   del_t = tf - ti
   # print time taken using divide-and-conquer approach
-
+  print('The time taken for recursive search is '+str(del_t)+'seconds.')
+  print()
   ti = time.time()
   # output greates path from dynamic programming 
+  great_sum=dynamic_prog(row_list)
+  print("The greatest path sum through dynamic programming is "+ str(great_sum) + '.')
   tf = time.time()
   del_t = tf - ti
   # print time taken using dynamic programming
-
+  print('The time taken for dynamic programming is '+str(del_t)+'seconds.')
 if __name__ == "__main__":
   main()
