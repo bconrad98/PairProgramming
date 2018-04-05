67b446c0f8e9ad3a65181bdd311eed1dc2b6ce28
Braeden Conrad <bconrad98@gmail.com>
2018-04-04 21:59:26 -0500

    Fixed things up

--- Josephus.py
+++ Josephus.py
@@ -16,7 +16,9 @@
 
 #  Date Created: 03/31/18
 
-#  Date Last Modified: 03/31/18
+#  Date Last Modified: 04/01/18
+
+# Creates list for printed order
 kill_list=[]
 
 class Link(object):
@@ -26,59 +28,68 @@ class Link(object):
 
 class CircularList(object):
 	def __init__(self):
-		self.head = Link(None)
-		self.head.next = self.head #head points to itself
+		self.first = Link(1)
+		self.first.next = self.first #head points to itself
 
+	# Inserts an element into the list
 	def insert(self,item):
+		cur=self.first
 		new_link = Link(item)
-		cur = self.head.next
-		while (cur.next != self.head):
-			cur = cur.next
-		cur.next = new_link
-		new_link.next = self.head
-
+		if(cur==None):
+			self.first=new_link
+			new_link.next=self.first
+		else:
+			while (cur.next != self.first):
+				cur = cur.next
+			new_link.next=cur.next
+			cur.next=new_link
+		
+	# Find the link with the given key
 	def find(self,key):
-		cur = self.head.next
-		while (cur != self.head):
+		cur = self.first
+		if (key == cur.data):
+			return cur
+		cur=cur.next
+		while (cur != self.first):
 			if (key == cur.data):
 				return cur
 			cur = cur.next
 		return None
 
+	# Deleta a link with a given key
 	def delete (self,key):
-		prev = self.head.next
-		cur = self.head.next
-		while (cur != self.head):
-			if (cur.data == key):
-				break
+		prev = self.first
+		cur = self.first.next
+		if(cur==None):
+			return None
+		while(cur.data!=key):
 			prev = cur
 			cur = cur.next
-		if (cur == self.head):
-			return None
-		prev.next = cur.next
+		if (cur == self.first):
+			self.first = cur.next
+			prev.next=cur.next
+		else:
+			prev.next = cur.next
 
+	# Delete the nth link starting from the Link start
+	# Return the next link from the deleted link
 	def delete_after(self,start,n): #this needs work
 		cur = start
 		for i in range(n-1):
 			cur = cur.next
-		del_link=cur.next
-		cur.next=del_link
-		if (cur == self.head):
-			self.head = cur.next
-		if (cur.next == self.head):
-			next_link = cur.next.next
-		else: 
-			next_link = cur.next
+		next_link=cur.next
 		kill_list.append(cur.data)
 		self.delete(cur.data)
 		
 		return next_link
 
+	# Return a string representation of a Circular list
 	def __str__(self):
-		cur=self.head.next
-		count = 0
-		string = ''
-		while (cur != self.head):
+		cur=self.first
+		count = 1
+		string = str(cur.data)+ ' '
+		cur=cur.next
+		while (cur != self.first):
 			count += 1
 			if (count > 10):
 				string += '\n'
@@ -90,24 +101,33 @@ class CircularList(object):
 		return string
 
 	def length(self):
-		cur = self.head.next
-		count = 0
-		while (cur != self.head):
+		cur = self.first
+		count = 1
+		cur=cur.next
+		while (cur != self.first):
 			count += 1
 			cur = cur.next
 		return count	
 
 def main():
+	# Creates object list and reads file
 	circular_list = CircularList()
 	in_file = open("josephus.txt",'r')
 	num_soldiers = int(in_file.readline().strip())
-	for i in range (1,num_soldiers+1):
+
+	# Creates the circular list of soldiers
+	for i in range (2,num_soldiers+1):
 		circular_list.insert(i)
 	start = circular_list.find(int(in_file.readline().strip()))
 	n = int(in_file.readline().strip())
+	
+	# Runs program to kill soldiers
 	while (circular_list.length() > 1):
 		start = circular_list.delete_after(start,n)
-		print(start.data-1)
-	print("Last element:",circular_list.head.next.data)
-
+	
+	# Print list of soldiers killed in order
+	for i in range (1,len(kill_list)):
+		print(kill_list[i])
+	print(circular_list.first.data)
+	
 main()
